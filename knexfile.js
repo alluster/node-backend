1aaff8dee4c69eb61ea89203c8d9a85265556a8f
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-06-03 17:47:35 +0300

    production knex config

--- knexfile.js
+++ knexfile.js
@@ -21,6 +21,26 @@ export const config = {
 		seeds: {
 			directory: './db/seeds',
 		},
+	},
+	production: {
+		client: 'pg',
+		debug: false,
+		connection: {
+			host: 'db',
+			database: process.env.POSTGRES_DB,
+			user: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			directory: './db/migrations',
+		},
+		seeds: {
+			directory: './db/seeds',
+		},
 	}
 };
 
