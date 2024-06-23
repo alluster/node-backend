c3a9a2951c857c96176ded2df84ff1ead707770b
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-06-23 13:19:53 +0300

    migration fix

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,7 @@ export const config = {
 		client: 'pg',
 		debug: true,
 		connection: {
-			host: 'db',
+			// host: 'db',
 			database: process.env.POSTGRES_DB,
 			user: process.env.POSTGRES_USER,
 			password: process.env.POSTGRES_PASSWORD,
