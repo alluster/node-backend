c1cf3ee7dc1c828f74a53c8ffd8d19ea07aac2e7
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-06-25 13:01:15 +0300

    migration fix

--- db/knex.js
+++ db/knex.js
@@ -1,5 +1,7 @@
 import knex from 'knex';
 import config from '../knexfile.js';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const environment = process.env.ENVIRONMENT || 'development';
 const knexConfig = config[environment];
