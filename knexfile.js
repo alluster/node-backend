dcf3bee5ed5d806d805d1b4ad410bf88aab130d7
“Aleksanteri” <aleksanteri.heliovaara@gmail.com>
2024-06-25 13:08:21 +0300

    migration fix

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,9 @@ dotenv.config();
 export const config = {
 	development: {
 		client: 'pg',
-		debug: true,
 		connection: {
-			host: 'localhost', // Change this if your database is hosted elsewhere
-			database: process.env.POSTGRES_DB,
-			user: process.env.POSTGRES_USER,
-			password: process.env.POSTGRES_PASSWORD,
-			port: 5432 // Ensure this port matches your PostgreSQL configuration
+			connectionString: `${process.env.DATABASE_URL}`,
+			ssl: { rejectUnauthorized: false },
 		},
 		pool: {
 			min: 2,
